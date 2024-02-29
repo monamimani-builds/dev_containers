dc552379bc5d33dea1c819bf8ec2bf21e54d3e53
monamimani <monamimani@gmail.com>
2024-02-29 18:00:01 -0500

    Moved the installation of packages in a bash script.

---
+++ src/cpp/.devcontainer/install-packages.sh
@@ -0,0 +1,28 @@
+#!/usr/bin/env bash
+
+apt-get update 
+apt-get upgrade -y
+sudo apt-get install -y --no-install-recommends software-properties-common
+
+pushd /tmp/
+wget https://apt.kitware.com/kitware-archive.sh
+chmod +x kitware-archive.sh
+sudo ./kitware-archive.sh
+sudo apt install -y cmake
+popd
+
+
+# vcpkg: https://github.com/microsoft/vcpkg/blob/master/README.md#quick-start-unix
+#RUN pushd "$VCPKG_ROOT"
+pushd $VCPKG_ROOT
+ls -al
+#&& git config --global --add safe.directory "$VCPKG_ROOT" 
+chown -R $(id -u):$(id -g) $PWD
+git pull --ff-only
+popd
+
+
+
+# Cleaning
+apt-get clean
+rm -rf /var/lib/apt/lists/*
