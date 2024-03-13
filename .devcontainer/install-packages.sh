436956a48f578e4c6e50bff086969ecf30dccd2f
monamimani <monamimani@gmail.com>
2024-03-13 00:10:06 +0000

    Fixing install-package script for alpine.

--- .devcontainer/install-packages.sh
+++ .devcontainer/install-packages.sh
@@ -1,28 +1,11 @@
 #!/usr/bin/env bash
 
-apt-get update 
-apt-get upgrade -y
-sudo apt-get install -y --no-install-recommends software-properties-common
-
-pushd /tmp/
-wget https://apt.kitware.com/kitware-archive.sh
-chmod +x kitware-archive.sh
-sudo ./kitware-archive.sh
-sudo apt install -y cmake
-popd
-
-# vcpkg: https://github.com/microsoft/vcpkg/blob/master/README.md#quick-start-unix
-# pushd $VCPKG_ROOT
-# git config --system --add safe.directory "$VCPKG_ROOT" 
-# git fetch --unshallow
-# git pull --ff-only
-# bootstrap-vcpkg.sh
-# popd
+apk update 
+apk upgrade -y
 
 
 
 # Cleaning
-apt-get remove -y --auto-remove software-properties-common
-apt-get autoremove -y
-apt-get clean
-rm -rf /var/lib/apt/lists/*
+apk autoremove -y
+apk cache clean
+rm -rf /var/cache/apk/*
