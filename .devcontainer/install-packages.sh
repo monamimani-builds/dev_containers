c00ef752e585a220a717f70995e148d6abecbda0
monamimani <monamimani@gmail.com>
2024-03-15 23:20:59 +0000

    Trying to fix devcontainer.

--- .devcontainer/install-packages.sh
+++ .devcontainer/install-packages.sh
@@ -2,35 +2,15 @@
 
 set -e
 
-apk update 
-apk add --upgrade apk-tools
-echo "upgrade"
-apk upgrade
-echo "add"
-apk add --upgrade \
-    git \
-    bash \
-    mandoc \
-    curl \
-    grep \
-    sudo \
-    docker \
+#!/usr/bin/env bash
+
+apt-get update 
+apt-get upgrade -y
+apt-get install -y docker
 
 
-# Ensure that login shells get the correct path if the user updated the PATH using ENV.
-rm -f /etc/profile.d/00-restore-env.sh
-echo "export PATH=${PATH//$(sh -lc 'echo $PATH')/\$PATH}" > /etc/profile.d/00-restore-env.sh
-chmod +x /etc/profile.d/00-restore-env.sh
-
-USERNAME=vscode
-USER_UID=1000
-USER_GID=1000
-addgroup --gid $USER_GID $USERNAME
-adduser -s /bin/bash --uid $USER_UID -G $USERNAME --disabled-password $USERNAME
-
-echo vscode ALL=\(root\) NOPASSWD:ALL > /etc/sudoers.d/$USERNAME
-chmod 0440 /etc/sudoers.d/$USERNAME
 
 # Cleaning
-apk cache clean
-rm -rf /var/cache/apk/*
+apt-get autoremove -y
+apt-get clean
+rm -rf /var/lib/apt/lists/*
