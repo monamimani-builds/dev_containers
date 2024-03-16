a8138702f48cce65681a7888a7eda37b7c1495e4
monamimani <monamimani@gmail.com>
2024-03-16 05:10:11 +0000

    Adding vcpkg to cppdevcontainer

--- src/cpp/.devcontainer/install-packages.sh
+++ src/cpp/.devcontainer/install-packages.sh
@@ -4,8 +4,8 @@ export DEBIAN_FRONTEND=noninteractive
 apt-get update 
 apt-get upgrade -y
 apt-get install -y --no-install-recommends git git-lfs sudo wget
-apt-get install -y --no-install-recommends software-properties-common
-apt-get install -y --no-install-recommends make ninja-build doxygen graphviz ccache cppcheck
+apt-get install -y --no-install-recommends software-properties-common build-essential pkg-config
+apt-get install -y --no-install-recommends ninja-build doxygen graphviz ccache cppcheck valgrind tar curl zip unzip
 
 pushd /tmp/
 echo "Install cmake"
@@ -24,21 +24,41 @@ LLVM_VER="18"
           libc++-${LLVM_VER}-dev libc++abi-${LLVM_VER}-dev 
 popd
 
-# # vcpkg: https://github.com/microsoft/vcpkg/blob/master/README.md#quick-start-unix
-# pushd $VCPKG_ROOT
-# git config --system --add safe.directory "$VCPKG_ROOT" 
-# git fetch --unshallow
-# git pull --ff-only
-# bootstrap-vcpkg.sh
-# popd
-
-
 # Set the default clang-tidy, so CMake can find it
 update-alternatives --install /usr/bin/clang-tidy clang-tidy $(which clang-tidy-${LLVM_VER}) 1
 
 # Set clang-${LLVM_VER} as default clang
 update-alternatives --install /usr/bin/clang clang $(which clang-${LLVM_VER}) 100
 update-alternatives --install /usr/bin/clang++ clang++ $(which clang++-${LLVM_VER}) 100
+
+# vcpkg: https://github.com/microsoft/vcpkg/blob/master/README.md#quick-start-unix
+mkdir -p "${VCPKG_ROOT}"
+mkdir -p "${VCPKG_DOWNLOADS}"
+pushd $VCPKG_ROOT
+git clone \
+    -c core.eol=lf \
+    -c core.autocrlf=false \
+    -c fsck.zeroPaddedFilemode=ignore \
+    -c fetch.fsck.zeroPaddedFilemode=ignore \
+    -c receive.fsck.zeroPaddedFilemode=ignore \
+    https://github.com/microsoft/vcpkg .
+
+git config --system --add safe.directory "$VCPKG_ROOT" 
+# git fetch --unshallow
+# git pull --ff-only
+bootstrap-vcpkg.sh
+popd
+
+# Add vcpkg to PATH
+updaterc "$(cat << EOF
+export VCPKG_ROOT="${VCPKG_ROOT}"
+if [[ "\${PATH}" != *"\${VCPKG_ROOT}"* ]]; then export PATH="\${PATH}:\${VCPKG_ROOT}"; fi
+EOF
+)"
+
+# Enable tab completion for bash and zsh
+VCPKG_FORCE_SYSTEM_BINARIES=1 su "${USERNAME}" -c "${VCPKG_ROOT}/vcpkg integrate bash"
+VCPKG_FORCE_SYSTEM_BINARIES=1 su "${USERNAME}" -c "${VCPKG_ROOT}/vcpkg integrate zsh"
 
 # Cleaning
 apt-get purge software-properties-common
