4a1ab7986028d22284bb5e0b563ea83f82bac604
monamimani <monamimani@gmail.com>
2024-03-16 17:21:38 +0000

    Making a script that set the git config identity on postStartCommand of the container.

--- .devcontainer/install-packages.sh
+++ .devcontainer/install-packages.sh
@@ -6,9 +6,6 @@ apt-get update
 apt-get -y upgrade
 apt-get -y install --no-install-recommends git apt-transport-https curl ca-certificates pigz iptables gnupg2 dirmngr wget jq
 
-git  config --global user.email "monamimani@gmail.com"
-git config --global user.name "monamimani"
-
 # Cleaning
 apt-get autoremove -y
 apt-get clean
