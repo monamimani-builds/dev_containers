20d7d196e8b55290860391060475804ef2dc8d8e
monamimani <monamimani@gmail.com>
2024-03-16 00:54:49 +0000

    Using diifferent way of naming the from image. maybe it does a difference. Using official Ubuntu image trying to make a devcontainer to compile cp and see after that if I can make one for alpine. Work in progress.

--- src/cpp/.devcontainer/install-packages.sh
+++ src/cpp/.devcontainer/install-packages.sh
@@ -2,22 +2,31 @@
 
 apt-get update 
 apt-get upgrade -y
-sudo apt-get install -y --no-install-recommends software-properties-common
+sudo apt-get install -y --no-install-recommends software-properties-common git git-lfs
 
 pushd /tmp/
+echo "Install cmake"
 wget https://apt.kitware.com/kitware-archive.sh
 chmod +x kitware-archive.sh
 sudo ./kitware-archive.sh
 sudo apt install -y cmake
+
+echo "Install LLVM"
+wget https://apt.llvm.org/llvm.sh
+chmod +x llvm.sh
+./llvm.sh all
+
+gcc --version
+clang --version
 popd
 
-# vcpkg: https://github.com/microsoft/vcpkg/blob/master/README.md#quick-start-unix
-pushd $VCPKG_ROOT
-git config --system --add safe.directory "$VCPKG_ROOT" 
-git fetch --unshallow
-git pull --ff-only
-bootstrap-vcpkg.sh
-popd
+# # vcpkg: https://github.com/microsoft/vcpkg/blob/master/README.md#quick-start-unix
+# pushd $VCPKG_ROOT
+# git config --system --add safe.directory "$VCPKG_ROOT" 
+# git fetch --unshallow
+# git pull --ff-only
+# bootstrap-vcpkg.sh
+# popd
 
 
 
