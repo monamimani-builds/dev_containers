01ec95bac188bc1373d46c4cfd4e4ec0477bc2f4
monamimani <monamimani@gmail.com>
2024-03-20 20:27:13 +0000

    Trying to fix the Container env.

--- src/cpp/.devcontainer/install-packages.sh
+++ src/cpp/.devcontainer/install-packages.sh
@@ -145,12 +145,13 @@ git fetch --unshallow
 git pull --ff-only
 bootstrap-vcpkg.sh
 
-source /etc/os-release
-wget -q https://packages.microsoft.com/config/ubuntu/$VERSION_ID/packages-microsoft-prod.deb
-dpkg -i packages-microsoft-prod.deb
-rm packages-microsoft-prod.deb
-apt-get update
-apt-get install -y powershell
+# Install Powershell
+# source /etc/os-release
+# wget -q https://packages.microsoft.com/config/ubuntu/$VERSION_ID/packages-microsoft-prod.deb
+# dpkg -i packages-microsoft-prod.deb
+# rm packages-microsoft-prod.deb
+# apt-get update
+# apt-get install -y powershell
 
 popd
 
