98c3fbcddb4c758c826138d0b4863a9ef844c338
monamimani <monamimani@gmail.com>
2024-03-28 22:31:43 -0400

    Trying to make it so that the install vcpkg in not cached

--- src/cpp/.devcontainer/install-packages.sh
+++ src/cpp/.devcontainer/install-packages.sh
@@ -121,29 +121,6 @@ done
 # update-alternatives --install /usr/bin/clang clang $(which clang-${LLVM_VER}) 100
 # update-alternatives --install /usr/bin/clang++ clang++ $(which clang++-${LLVM_VER}) 100
 
-# vcpkg: https://github.com/microsoft/vcpkg/blob/master/README.md#quick-start-unix
-mkdir -p "${VCPKG_ROOT}"
-mkdir -p "${VCPKG_DOWNLOADS}"
-pushd $VCPKG_ROOT
-SHALLOW_CLONE_DATE=$(date -d "-1 years" +%s)
-git clone \
-    --depth=1 \
-    -c core.eol=lf \
-    -c core.autocrlf=false \
-    -c fsck.zeroPaddedFilemode=ignore \
-    -c fetch.fsck.zeroPaddedFilemode=ignore \
-    -c receive.fsck.zeroPaddedFilemode=ignore \
-    https://github.com/microsoft/vcpkg .
-
-    #--shallow-since=${SHALLOW_CLONE_DATE} \
-    # --single-branch \
-    # --branch=master \
-    # --no-tags \
-
-git config --system --add safe.directory "$VCPKG_ROOT" 
-git fetch --unshallow
-git pull --ff-only
-bootstrap-vcpkg.sh
 
 # Install Powershell
 # source /etc/os-release
@@ -153,7 +130,6 @@ bootstrap-vcpkg.sh
 # apt-get update
 # apt-get install -y powershell
 
-popd
 
 # Add to bashrc/zshrc files for all users.
 updaterc() {
@@ -166,17 +142,6 @@ updaterc() {
     fi
 }
 
-
-# Add vcpkg to PATH
-updaterc "$(cat << EOF
-export VCPKG_ROOT="${VCPKG_ROOT}"
-if [[ "\${PATH}" != *"\${VCPKG_ROOT}"* ]]; then export PATH="\${PATH}:\${VCPKG_ROOT}"; fi
-EOF
-)"
-
-# Enable tab completion for bash and zsh
-VCPKG_FORCE_SYSTEM_BINARIES=1 su "vscode" -c "${VCPKG_ROOT}/vcpkg integrate bash"
-VCPKG_FORCE_SYSTEM_BINARIES=1 su "root" -c "${VCPKG_ROOT}/vcpkg integrate bash"
 
 # Cleaning
 echo "Cleanup"
---
+++ src/cpp/.devcontainer/install-vcpkg.sh
@@ -0,0 +1,39 @@
+#!/usr/bin/env bash
+
+set -eux
+
+# vcpkg: https://github.com/microsoft/vcpkg/blob/master/README.md#quick-start-unix
+mkdir -p "${VCPKG_ROOT}"
+mkdir -p "${VCPKG_DOWNLOADS}"
+pushd $VCPKG_ROOT
+SHALLOW_CLONE_DATE=$(date -d "-1 years" +%s)
+git clone \
+    --depth=1 \
+    -c core.eol=lf \
+    -c core.autocrlf=false \
+    -c fsck.zeroPaddedFilemode=ignore \
+    -c fetch.fsck.zeroPaddedFilemode=ignore \
+    -c receive.fsck.zeroPaddedFilemode=ignore \
+    https://github.com/microsoft/vcpkg .
+
+    #--shallow-since=${SHALLOW_CLONE_DATE} \
+    # --single-branch \
+    # --branch=master \
+    # --no-tags \
+
+git config --system --add safe.directory "$VCPKG_ROOT" 
+git fetch --unshallow
+git pull --ff-only
+bootstrap-vcpkg.sh
+popd
+
+# Add vcpkg to PATH
+updaterc "$(cat << EOF
+export VCPKG_ROOT="${VCPKG_ROOT}"
+if [[ "\${PATH}" != *"\${VCPKG_ROOT}"* ]]; then export PATH="\${PATH}:\${VCPKG_ROOT}"; fi
+EOF
+)"
+
+# Enable tab completion for bash and zsh
+VCPKG_FORCE_SYSTEM_BINARIES=1 su "vscode" -c "${VCPKG_ROOT}/vcpkg integrate bash"
+VCPKG_FORCE_SYSTEM_BINARIES=1 su "root" -c "${VCPKG_ROOT}/vcpkg integrate bash"
